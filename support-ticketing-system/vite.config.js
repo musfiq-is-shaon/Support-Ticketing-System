faae6f64b5b84a2c8611a99f6afcced2a9e244c8
musfiq-is-shaon <musfiqul.islam989@gmai.com>
2026-02-02 21:09:30 +0600

    fixes

--- support-ticketing-system/vite.config.js
+++ support-ticketing-system/vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: './',
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+  },
+  server: {
+    port: 5173,
+  }
 })
 
